2f6743d3e15b76e38cc8552effe6a609c00c0018
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-08-25 17:24:52 -0500

    genetic programming description heart done

--- gp_overview/main.py
+++ gp_overview/main.py
@@ -144,4 +144,87 @@ class TitleSlide(Scene):
 # Corresponds to slide 2 in brainstorming notes
 class GeneticProgrammingDescription(Scene):
     def construct(self):
-        pass
+        circle_radius: float = 0.7
+        circle_font_size: int = 30
+
+        full_code = Text("print(1 + 2 - 4)")
+        print_circle = Circle(radius=circle_radius, color=BLUE)
+        print_text = Text("print", font_size=circle_font_size)
+        minus_circle = Circle(radius=circle_radius, color=BLUE)
+        minus_text = Text("-", font_size=circle_font_size)
+        four_minus_circle = Circle(radius=circle_radius, color=BLUE)
+        four_minus_text = Text("4", font_size=circle_font_size)
+        plus_circle = Circle(radius=circle_radius, color=BLUE)
+        plus_text = Text("+", font_size=circle_font_size)
+        left_one_circle = Circle(radius=circle_radius, color=BLUE)
+        left_one_text = Text("1", font_size=circle_font_size)
+        right_two_circle = Circle(radius=circle_radius, color=BLUE)
+        right_two_text = Text("2", font_size=circle_font_size)
+
+        full_code.shift(LEFT * 4)
+
+        print_circle.shift(UP * 3 + RIGHT * 2.5)
+        print_text.move_to(print_circle.get_center())
+        print_text.add_updater(lambda x: x.move_to(print_circle.get_center()))
+
+        minus_circle.shift(UP + RIGHT * 2.5)
+        minus_text.move_to(minus_circle.get_center())
+        minus_text.add_updater(lambda x: x.move_to(minus_circle.get_center()))
+
+        four_minus_circle.shift(DOWN * 0.5 + RIGHT * 4.0)
+        four_minus_text.move_to(four_minus_circle.get_center())
+        four_minus_text.add_updater(lambda x: x.move_to(four_minus_circle.get_center()))
+
+        plus_circle.shift(DOWN * 0.5 + RIGHT)
+        plus_text.move_to(four_minus_circle.get_center())
+        plus_text.add_updater(lambda x: x.move_to(plus_circle.get_center()))
+
+        left_one_circle.shift(DOWN * 2.5 + LEFT * 1.0)
+        left_one_text.move_to(left_one_circle.get_center())
+        left_one_text.add_updater(lambda x: x.move_to(left_one_circle.get_center()))
+
+        right_two_circle.shift(DOWN * 2.5 + RIGHT * 3.0)
+        right_two_text.move_to(right_two_circle.get_center())
+        right_two_text.add_updater(lambda x: x.move_to(right_two_circle.get_center()))
+
+        # Create lines after all mobjects shifted
+        print_minus_line = Line(print_circle, minus_circle)
+        minus_plus_line = Line(minus_circle, plus_circle)
+        minus_four_line = Line(minus_circle, four_minus_circle)
+        plus_one_line = Line(plus_circle, left_one_circle)
+        plus_two_line = Line(plus_circle, right_two_circle)
+        
+        self.play(
+            Write(full_code),
+            run_time=3
+        )
+        self.play(
+            Write(print_circle), 
+            Write(minus_circle),
+            Write(four_minus_circle),
+            Write(plus_circle),
+            Write(left_one_circle),
+            Write(right_two_circle),
+            run_time=3
+        )
+        self.play(
+            Write(print_minus_line),
+        )
+        self.play(
+            Write(minus_four_line),
+            Write(minus_plus_line),
+        )
+        self.play(
+            Write(plus_one_line),
+            Write(plus_two_line),
+        )
+        self.play(
+            Write(print_text),
+            Write(minus_text),
+            Write(four_minus_text),
+            Write(plus_text),
+            Write(left_one_text),
+            Write(right_two_text),
+            run_time = 2
+        )
+        print_text.clear_updaters()
