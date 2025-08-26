bd31b4bb9f10894ecbcc00e017d4c4550f7913be
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-08-26 16:04:14 -0500

    grow & full done

--- gp_overview/main.py
+++ gp_overview/main.py
@@ -304,9 +304,9 @@ while (loop count < threshold) do
         # tree depth reached. Beyond this limit, only terminals
         # can be selected.
         # Only going to a depth of 2 here
+        # Can think about this as a depth first creation of sorts
 
-        full_text = Text("Full")
-        full_text.to_edge(UR)
+        full_text = Text("Full").to_edge(UR)
 
         def gen_time_text(time: int | str) -> Text:
             return Text(f"t={time}", color=GREEN).next_to(full_text, LEFT)
@@ -327,10 +327,9 @@ while (loop count < threshold) do
 
         root_node = Circle(radius=circle_radius, color=BLUE).shift(UP * 3 + RIGHT)
         root_text = Text("+", font_size=circle_font_size)
-        root_text.add_updater(lambda x: x.move_to(root_node.get_center()))
+        root_text.move_to(root_node.get_center())
 
-        self.play(Write(root_node))
-        self.play(Write(root_text))
+        self.play(Write(root_node), Write(root_text))
 
         # end t=1
         # start t=2
@@ -341,9 +340,8 @@ while (loop count < threshold) do
             root_node, DOWN + LEFT
         )
         mult_text = Text("*", font_size=circle_font_size)
-        mult_text.add_updater(lambda x: x.move_to(mult_node.get_center()))
-        self.play(Write(mult_node))
-        self.play(Write(mult_text))
+        mult_text.move_to(mult_node.get_center())
+        self.play(Write(mult_node), Write(mult_text))
 
         root_mult_line = Line(root_node, mult_node)
         self.play(Write(root_mult_line))
@@ -357,9 +355,212 @@ while (loop count < threshold) do
             mult_node, DOWN + LEFT
         )
         x_text = Text("x", font_size=circle_font_size)
-        x_text.add_updater(lambda x: x.move_to(x_node.get_center()))
-        self.play(Write(x_node))
-        self.play(Write(x_text))
+        x_text.move_to(x_node.get_center())
+        self.play(Write(x_node), Write(x_text))
 
         mult_x_line = Line(mult_node, x_node)
         self.play(Write(mult_x_line))
+
+        # end t=3
+        # start t=4
+        time4_text = gen_time_text(4)
+        self.play(TransformMatchingShapes(time3_text, time4_text))
+
+        y_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            mult_node, DOWN * 2
+        )
+        y_text = Text("y", font_size=circle_font_size)
+        y_text.move_to(y_node.get_center())
+        self.play(Write(y_node), Write(y_text))
+
+        mult_y_line = Line(mult_node, y_node)
+        self.play(Write(mult_y_line))
+
+        # end t=4
+        # start t=5
+        time5_text = gen_time_text(5)
+        self.play(TransformMatchingShapes(time4_text, time5_text))
+
+        div_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            root_node, DOWN + RIGHT
+        )
+        div_text = Text("/", font_size=circle_font_size)
+        div_text.move_to(div_node.get_center())
+        self.play(Write(div_text), Write(div_node))
+
+        root_div_line = Line(root_node, div_node)
+        self.play(Write(root_div_line))
+
+        # end t=5
+        # start t=6
+        time6_text = gen_time_text(6)
+        self.play(TransformMatchingShapes(time5_text, time6_text))
+
+        one_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            div_node, DOWN * 2
+        )
+        one_text = Text("1", font_size=circle_font_size)
+        one_text.move_to(one_node.get_center())
+        self.play(Write(one_text), Write(one_node))
+
+        div_one_line = Line(div_node, one_node)
+        self.play(Write(div_one_line))
+
+        # end t=6
+        # start t=7
+        time7_text = gen_time_text(7)
+        self.play(TransformMatchingShapes(time6_text, time7_text))
+
+        zero_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            div_node, DOWN + RIGHT
+        )
+        zero_text = Text("0", font_size=circle_font_size)
+        zero_text.move_to(zero_node.get_center())
+        self.play(Write(zero_text), Write(zero_node))
+
+        div_zero_line = Line(div_node, zero_node)
+        self.play(Write(div_zero_line))
+
+        # end t=7
+        # fade the full tree out
+        full_group = VGroup(
+            root_node,
+            root_text,
+            mult_node,
+            mult_text,
+            div_node,
+            div_text,
+            x_node,
+            x_text,
+            y_node,
+            y_text,
+            one_node,
+            one_text,
+            zero_node,
+            zero_text,
+            root_mult_line,
+            root_div_line,
+            mult_x_line,
+            mult_y_line,
+            div_one_line,
+            div_zero_line
+        )
+        self.play(Unwrite(full_group), TransformMatchingShapes(time7_text, time0_text))
+
+        # Onto grow
+        # Like full but random chance to be a terminal anywhere
+        # in the tree.
+        grow_text = Text("Grow").to_edge(UR)
+        self.play(
+            TransformMatchingShapes(full_text, grow_text),
+            time0_text.animate.next_to(grow_text, LEFT)
+        )
+
+        # start t=1
+        time1_text.next_to(grow_text, LEFT)
+        self.play(TransformMatchingShapes(time0_text, time1_text))
+
+        root_node = Circle(radius=circle_radius, color=BLUE).shift(UP * 3 + RIGHT)
+        root_text = Text("+", font_size=circle_font_size)
+        root_text.move_to(root_node.get_center())
+
+        self.play(Write(root_node), Write(root_text))
+
+        # end t=1
+        # start t=2
+        time2_text.next_to(grow_text, LEFT)
+        self.play(TransformMatchingShapes(time1_text, time2_text))
+        
+        x_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            root_node, DOWN + LEFT
+        )
+        x_text = Text("x", font_size=circle_font_size)
+        x_text.move_to(x_node.get_center())
+        self.play(Write(x_node), Write(x_text))
+
+        root_x_line = Line(root_node, x_node)
+        self.play(Write(root_x_line))
+
+        # end t=2
+        # start t=3
+        time3_text.next_to(grow_text, LEFT)
+        self.play(TransformMatchingShapes(time2_text, time3_text))
+
+        minus_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            root_node, DOWN + RIGHT
+        )
+        minus_text = Text("-", font_size=circle_font_size)
+        minus_text.move_to(minus_node.get_center())
+        self.play(Write(minus_text), Write(minus_node))
+
+        root_minus_line = Line(root_node, minus_node)
+        self.play(Write(root_minus_line))
+
+        # end t=3
+        # start t=4 and t=5
+        time5_text.next_to(grow_text, LEFT)
+        self.play(TransformMatchingShapes(time3_text, time5_text))
+
+        one_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            minus_node, DOWN * 2
+        )
+        one_text = Text("1", font_size=circle_font_size)
+        one_text.move_to(one_node.get_center())
+        minus_one_line = Line(minus_node, one_node)
+
+        zero_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            minus_node, DOWN + RIGHT
+        )
+        zero_text = Text("0", font_size=circle_font_size)
+        zero_text.move_to(zero_node.get_center())
+        minus_zero_line = Line(minus_node, zero_node)
+
+        self.play(
+            Write(one_text), 
+            Write(one_node),
+            Write(zero_node),
+            Write(zero_text)
+        )
+        self.play(
+            Write(minus_one_line),
+            Write(minus_zero_line)
+        )
+
+        # end t=4 and t=5
+        grow_group = VGroup(
+            root_node,
+            root_text,
+            x_node,
+            x_text,
+            minus_node,
+            minus_text,
+            one_node,
+            one_text,
+            zero_node,
+            zero_text,
+            root_x_line,
+            root_minus_line,
+            minus_one_line,
+            minus_zero_line
+        )
+        self.play(Unwrite(grow_group), 
+                  Unwrite(time5_text),
+                  Unwrite(depth_text),
+                  Unwrite(grow_text),
+        )
+
+        # Need to mention ramped half and half somewhere
+        # First half constructed with full then rest constructed
+        # with grow
+        rhandh_text = Text("Ramped Half-and-Half")
+        rhandh_description_text = Text("Generate 1st half with Full and 2nd half with Grow", font_size=20).next_to(rhandh_text, DOWN)
+        self.play(Write(rhandh_text), Write(rhandh_description_text))
+        # probably put a slide split here for later
+        self.play(FadeOut(rhandh_text), FadeOut(rhandh_description_text))
+
+        # Switch to rank population next
+        self.play(
+            pseudocode_mobject[:28].animate.set_color(WHITE),
+            pseudocode_mobject[28:42].animate.set_color(BLUE)    
+        )
+
