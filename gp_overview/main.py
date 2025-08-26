ed20c6fd4e4e730f576741c40ec82a5681aba99d
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-08-26 13:12:56 -0500

    full method partially finished

--- gp_overview/main.py
+++ gp_overview/main.py
@@ -7,6 +7,7 @@ class CreateCircle(Scene):
         circle.set_fill(PINK, opacity=0.3)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
 
+
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()
@@ -19,6 +20,7 @@ class SquareToCircle(Scene):
         self.play(Transform(square, circle))
         self.play(FadeOut(square))
 
+
 class SquareAndCircle(Scene):
     def construct(self):
         circle = Circle()
@@ -30,6 +32,7 @@ class SquareAndCircle(Scene):
         square.next_to(circle, RIGHT, buff=0.5)
         self.play(Create(circle), Create(square))
 
+
 class AnimatedSquareToCircle(Scene):
     def construct(self):
         circle = Circle()
@@ -38,9 +41,8 @@ class AnimatedSquareToCircle(Scene):
         self.play(Create(square))
         self.play(square.animate.rotate(PI / 4))
         self.play(Transform(square, circle))
-        self.play(
-            square.animate.set_fill(PINK, opacity=0.5)
-        )
+        self.play(square.animate.set_fill(PINK, opacity=0.5))
+
 
 class DifferentRotations(Scene):
     def construct(self):
@@ -51,12 +53,14 @@ class DifferentRotations(Scene):
         )
         self.wait()
 
+
 class HelloWorld(Scene):
     def construct(self):
         text = Text("Hello World", font_size=144)
         # How to slow the .play function down?
         self.play(Write(text), run_time=2.0)
         self.wait(3)
+
 
 class TwoTransform(Scene):
     def transform(self):
@@ -66,7 +70,6 @@ class TwoTransform(Scene):
         self.play(Transform(a, b))
         self.play(Transform(a, c))
         self.play(FadeOut(a))
-
 
     def replacement_transform(self):
         a = Circle()
@@ -82,6 +85,7 @@ class TwoTransform(Scene):
         self.wait(0.5)
         self.replacement_transform()
 
+
 class Shapes(Scene):
     def construct(self):
         circle = Circle()
@@ -94,6 +98,7 @@ class Shapes(Scene):
 
         self.add(circle, square, triangle)
         self.wait(1)
+
 
 class MobjectPlacement(Scene):
     def construct(self):
@@ -108,7 +113,9 @@ class MobjectPlacement(Scene):
         self.add(circle, square, triangle)
         self.wait(1)
 
+
 # Start of GP presentation
+
 
 class QuoteSlide(Scene):
     def construct(self):
@@ -117,11 +124,23 @@ class QuoteSlide(Scene):
         # quote from: https://en.wikisource.org/wiki/Mendel%27s_Principles_of_Heredity;_a_Defence/Chapter_2
         # Paper Title: Experiments in Plant-Hybridisation
         # Translated from the original 1865 paper in 1901
-        text0 = Text("\"...it is now clear that the hybrids form seeds having", font_size=font_size, slant=ITALIC)
-        text1 = Text("one or other of the two differentiating characters, and of these one-half", font_size=font_size, slant=ITALIC)
-        text2 = Text("develop again the hybrid form...\"", font_size=font_size, slant=ITALIC)
-        gregor_mendel = Text("-Gregor Mendel", font_size=font_size + 10, weight=BOLD, color=BLUE)
-        
+        text0 = Text(
+            '"...it is now clear that the hybrids form seeds having',
+            font_size=font_size,
+            slant=ITALIC,
+        )
+        text1 = Text(
+            "one or other of the two differentiating characters, and of these one-half",
+            font_size=font_size,
+            slant=ITALIC,
+        )
+        text2 = Text(
+            'develop again the hybrid form..."', font_size=font_size, slant=ITALIC
+        )
+        gregor_mendel = Text(
+            "-Gregor Mendel", font_size=font_size + 10, weight=BOLD, color=BLUE
+        )
+
         # orient the text Mobjects
         text0.shift(UP * 1.5)
         text1.next_to(text0, DOWN)
@@ -131,9 +150,12 @@ class QuoteSlide(Scene):
         self.play(Write(text0), Write(text1), Write(text2), run_time=3.0)
         self.play(Write(gregor_mendel), run_time=2.0)
 
+
 class TitleSlide(Scene):
     def construct(self):
-        title = Text("An Overview of Genetic Programming: Tree-Based GP and PushGP", font_size=30)
+        title = Text(
+            "An Overview of Genetic Programming: Tree-Based GP and PushGP", font_size=30
+        )
         name = Text("Rowan Torbitzky-Lane", font_size=25, color=BLUE)
 
         name.next_to(title, DOWN)
@@ -141,13 +163,14 @@ class TitleSlide(Scene):
         self.play(Write(title), run_time=3.0)
         self.play(Write(name), run_time=2.0)
 
+
 # Corresponds to slide 2 in brainstorming notes
 class GeneticProgrammingDescription(Scene):
     def construct(self):
         circle_radius: float = 0.7
         circle_font_size: int = 30
 
-        full_code = Text("print(1 + 2 - 4)")
+        full_code = Text("print((1 + 2) - 4)")
         print_circle = Circle(radius=circle_radius, color=BLUE)
         print_text = Text("print", font_size=circle_font_size)
         minus_circle = Circle(radius=circle_radius, color=BLUE)
@@ -179,11 +202,11 @@ class GeneticProgrammingDescription(Scene):
         plus_text.move_to(four_minus_circle.get_center())
         plus_text.add_updater(lambda x: x.move_to(plus_circle.get_center()))
 
-        left_one_circle.shift(DOWN * 2.5 + LEFT * 1.0)
+        left_one_circle.shift(DOWN * 2.0 + LEFT * 0.5)
         left_one_text.move_to(left_one_circle.get_center())
         left_one_text.add_updater(lambda x: x.move_to(left_one_circle.get_center()))
 
-        right_two_circle.shift(DOWN * 2.5 + RIGHT * 3.0)
+        right_two_circle.shift(DOWN * 2.0 + RIGHT * 2.5)
         right_two_text.move_to(right_two_circle.get_center())
         right_two_text.add_updater(lambda x: x.move_to(right_two_circle.get_center()))
 
@@ -193,19 +216,16 @@ class GeneticProgrammingDescription(Scene):
         minus_four_line = Line(minus_circle, four_minus_circle)
         plus_one_line = Line(plus_circle, left_one_circle)
         plus_two_line = Line(plus_circle, right_two_circle)
-        
+
+        self.play(Write(full_code), run_time=3)
         self.play(
-            Write(full_code),
-            run_time=3
-        )
-        self.play(
-            Write(print_circle), 
+            Write(print_circle),
             Write(minus_circle),
             Write(four_minus_circle),
             Write(plus_circle),
             Write(left_one_circle),
             Write(right_two_circle),
-            run_time=3
+            run_time=3,
         )
         self.play(
             Write(print_minus_line),
@@ -225,6 +245,121 @@ class GeneticProgrammingDescription(Scene):
             Write(plus_text),
             Write(left_one_text),
             Write(right_two_text),
-            run_time = 2
+            run_time=2,
         )
-        print_text.clear_updaters()
+
+        # Throughout the populations, the AST changes
+        # The actual program itself could change too
+        # This section animates those
+        mult_text = Text("*", font_size=circle_font_size).move_to(minus_text)
+        log_text = Text("log", font_size=circle_font_size).move_to(print_text)
+        big_num_text = Text("9999", font_size=circle_font_size).move_to(right_two_text)
+        full_mult_text = Text("print((1 + 2) * 4)").move_to(full_code)
+        full_log_text = Text("log((1 + 2) * 4)").move_to(full_code)
+        full_big_num_text = Text("log((1 + 9999) * 4)").move_to(full_code)
+
+        self.play(
+            Transform(minus_text, mult_text),
+            TransformMatchingShapes(full_code, full_mult_text),
+        )
+        self.play(
+            Transform(print_text, log_text),
+            TransformMatchingShapes(full_mult_text, full_log_text),
+        )
+        self.play(
+            Transform(right_two_text, big_num_text),
+            TransformMatchingShapes(full_log_text, full_big_num_text),
+        )
+
+
+class ECLoop(Scene):
+    def construct(self):
+        circle_radius: float = 0.7
+        circle_font_size: int = 30
+
+        # EC loop pseudocode
+        # see the best possible individual in minimization problems personally
+        pseudocode: str = """
+initialize population randomly
+rank population
+while (loop count < threshold) do
+    select parents for reproduction
+    generate children from selected parents
+    evaluate children
+    select individuals for the next population
+    loop counter++
+"""
+        pseudocode_mobject = Text(pseudocode, font_size=30)
+        self.play(Write(pseudocode_mobject))
+        self.play(
+            pseudocode_mobject.animate.scale(0.5).to_edge(UL),
+        )
+        self.play(pseudocode_mobject[:28].animate.set_color(BLUE))
+
+        # Types program initialization
+        # Grow, Full, and Ramped Half&Half
+
+        # Full first
+        # Nodes taken at random from a function set until maximum
+        # tree depth reached. Beyond this limit, only terminals
+        # can be selected.
+        # Only going to a depth of 2 here
+
+        full_text = Text("Full")
+        full_text.to_edge(UR)
+
+        def gen_time_text(time: int | str) -> Text:
+            return Text(f"t={time}", color=GREEN).next_to(full_text, LEFT)
+
+        self.play(Write(full_text))
+
+        depth_text = Text("Max Depth=2", font_size=20)
+        depth_text.next_to(full_text, DOWN)
+        self.play(Write(depth_text))
+
+        time0_text = gen_time_text(0)
+
+        self.play(Write(time0_text))
+
+        # Start of t=1
+        time1_text = gen_time_text(1)
+        self.play(TransformMatchingShapes(time0_text, time1_text))
+
+        root_node = Circle(radius=circle_radius, color=BLUE).shift(UP * 3 + RIGHT)
+        root_text = Text("+", font_size=circle_font_size)
+        root_text.add_updater(lambda x: x.move_to(root_node.get_center()))
+
+        self.play(Write(root_node))
+        self.play(Write(root_text))
+
+        # end t=1
+        # start t=2
+        time2_text = gen_time_text(2)
+        self.play(TransformMatchingShapes(time1_text, time2_text))
+
+        mult_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            root_node, DOWN + LEFT
+        )
+        mult_text = Text("*", font_size=circle_font_size)
+        mult_text.add_updater(lambda x: x.move_to(mult_node.get_center()))
+        self.play(Write(mult_node))
+        self.play(Write(mult_text))
+
+        root_mult_line = Line(root_node, mult_node)
+        self.play(Write(root_mult_line))
+
+        # end t=2
+        # start t=3
+        time3_text = gen_time_text(3)
+        self.play(TransformMatchingShapes(time2_text, time3_text))
+
+        x_node = Circle(radius=circle_radius, color=BLUE).next_to(
+            mult_node, DOWN + LEFT
+        )
+        x_text = Text("x", font_size=circle_font_size)
+        x_text.add_updater(lambda x: x.move_to(x_node.get_center()))
+        self.play(Write(x_node))
+        self.play(Write(x_text))
+
+        mult_x_line = Line(mult_node, x_node)
+        self.play(Write(mult_x_line))
