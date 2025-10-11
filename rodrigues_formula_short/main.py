72f2fbf2749446739c1591d2c9aff660b8ff2b99
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-11 12:42:56 -0500

    formula and settings good

--- rodrigues_formula_short/main.py
+++ rodrigues_formula_short/main.py
@@ -1,5 +1,11 @@
 from manim import *
 
+
 class FormulaScene(Scene):
     def construct(self):
-        pass
+        rodrigues_formula: MathTex = MathTex(
+            r"\mathbf{R}(\hat{n}, \theta) = I + \sin\theta[\mathbf{\hat{n}}]_\times + (1 - \cos\theta)[\mathbf{\hat{n}}]_\times^2"
+        ).scale(0.5)
+        self.play(Write(rodrigues_formula), run_time=2)
+        self.wait()
+        self.play(Unwrite(rodrigues_formula), run_time=2)
