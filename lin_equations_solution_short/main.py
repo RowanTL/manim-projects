216fb22703f055040b907dd386e41d1da0b8ada0
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-10-28 18:10:50 -0500

    Linear equations text done

--- lin_equations_solution_short/main.py
+++ lin_equations_solution_short/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from lin-equations-solution-short!")
+from manim import *
+from typing import Final
+
+GLOBAL_SCALE: Final[float] = 0.45
 
 
-if __name__ == "__main__":
-    main()
+class LinEqSolutions(Scene):
+    def construct(self):
+        text_scale: float = 0.40
+        inf_text: Text = Text("Infinitely many points").scale(text_scale).to_edge(UP)
+        one_point_text: Text = (
+            Text("Exactly one point", color=YELLOW)
+            .scale(text_scale)
+            .next_to(inf_text, DOWN)
+        )
+        no_text: Text = (
+            Text("No points").scale(text_scale).next_to(one_point_text, DOWN)
+        )
+
+        self.play(Write(inf_text), Write(one_point_text), Write(no_text))
+        self.wait()
+        self.play(Unwrite(inf_text), Unwrite(one_point_text), Unwrite(no_text))
