c0322b97e2315065190b0edf64b0ca7b1fab7545
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-11-16 11:38:56 -0600

    start of a good presentation

--- actix_web_presentation/main.py
+++ actix_web_presentation/main.py
@@ -3,13 +3,85 @@ from manim_slides.slide import Slide
 
 config.frame_rate = 15
 
+
+# A slide that gives a general overview on how actix web actually works
+class OverviewSlide(Slide):
+    def construct(self):
+        # Scene setup
+        # ...
+
+        # Definitions
+        pi_user: SVGMobject = SVGMobject("assets/PiCreatures_erm.svg").to_edge(LEFT)
+
+        box_scale: float = 0.7
+        http_server_box: Square = Square(pi_user.width).to_edge(RIGHT)
+        http_server_text: Text = (
+            Text("HttpServer").next_to(http_server_box, UP * 0.5).scale(box_scale)
+        )
+        app_text: Text = Text("App").move_to(http_server_box).scale(box_scale)
+
+        arrow_text_scale = 0.5
+        http_request_arrow: Arrow = Arrow(
+            start=pi_user.get_corner(DR),
+            end=http_server_box.get_corner(DL),
+            color=BLUE,
+            stroke_width=2,
+        )
+        http_request_text: Text = (
+            Text("HttpRequest")
+            .next_to(http_request_arrow, DOWN * 0.1)
+            .scale(arrow_text_scale)
+        )
+
+        http_response_arrow: Arrow = Arrow(
+            end=pi_user.get_corner(UR),
+            start=http_server_box.get_corner(UL),
+            color=BLUE,
+            stroke_width=2,
+        )
+        http_response_text: Text = (
+            Text("HttpResponse")
+            .next_to(http_response_arrow, UP * 0.1)
+            .scale(arrow_text_scale)
+        )
+
+        # Animations
+        self.play(
+            FadeIn(pi_user),
+            FadeIn(http_server_box),
+            FadeIn(http_server_text),
+            FadeIn(app_text),
+        )
+        self.next_slide()
+        self.play(
+            Write(http_request_arrow),
+            Write(http_request_text),
+            Write(http_response_arrow),
+            Write(http_response_text),
+            run_time=2,
+        )
+        self.next_slide()
+        self.play(
+            FadeOut(pi_user),
+            FadeOut(http_server_box),
+            FadeOut(http_server_text),
+            FadeOut(app_text),
+            Unwrite(http_request_arrow),
+            Unwrite(http_request_text),
+            Unwrite(http_response_arrow),
+            Unwrite(http_response_text),
+        )
+        self.wait(0.5)
+        self.next_slide()
+
+
 # Need a slide describing user HttpRequest and Response
 # Basically go over responder implementation and how to do it
 # https://docs.rs/actix-web/latest/actix_web/trait.Responder.html
 
 
 # All of these types implement Responder
-class ResponderSlide(Scene):
+class ResponderSlide(Slide):
     def construct(self):
         # Scene setup
         # ...
@@ -49,4 +121,14 @@ class ResponderSlide(Scene):
             Write(http_response_text),
         )
         self.play(Write(arrows), run_time=2)
-        self.wait(2)
+
+        self.next_slide()
+        self.play(
+            Unwrite(responder_text),
+            Unwrite(cow_text),
+            Unwrite(vec_u8_text),
+            Unwrite(http_response_text),
+            Unwrite(arrows),
+        )
+        self.wait(0.5)
+        self.next_slide()
