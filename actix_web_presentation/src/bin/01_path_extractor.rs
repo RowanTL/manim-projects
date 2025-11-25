bd80c76bd140e175f33a6f6a64b20d332b73ca85
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-11-25 14:06:55 -0600

    Path extractor looking good

---
+++ actix_web_presentation/src/bin/01_path_extractor.rs
@@ -0,0 +1,38 @@
+use actix_web::{App, HttpServer, Result, get, web};
+
+/// extract path info from "/users/{user_id}/{friend}" url
+/// {user_id} - deserializes to a u32
+/// {friend} - deserializes to a String
+#[get("/users/{user_id}/{friend}")] // <- define path parameters
+async fn index(path: web::Path<(u32, String)>) -> Result<String> {
+    let (user_id, friend) = path.into_inner();
+    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{App, body::MessageBody, test};
+    use bytes::Bytes;
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_index_get() {
+        let app = test::init_service(App::new().service(index)).await;
+        let request = test::TestRequest::get().uri("/users/1/jerry").to_request();
+        let response = test::call_service(&app, request).await;
+        let res_body = response.into_body();
+        assert!(res_body.try_into_bytes().unwrap() == Bytes::from("Welcome jerry, user_id 1!"));
+        // let body_bytes = test::read_body(response).await;
+        // let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        // assert_eq!(body_str, "Hello world!");
+    }
+}
