ed7fe1a88393becb3a93044065806b0d002b43be
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-11-29 13:47:14 -0600

    add tests for scope

--- actix_web_presentation/src/bin/00_getting_started.rs
+++ actix_web_presentation/src/bin/00_getting_started.rs
@@ -14,11 +14,25 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+async fn all_users() -> impl Responder {
+    HttpResponse::Ok().body("All users!")
+}
+
+async fn user_info() -> impl Responder {
+    HttpResponse::Ok().body("Info on users!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // let user_scope = web::scope("/users").service(all_users).service(user_info);
+
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(
+                web::scope("/users")
+                    .route("/all", web::get().to(all_users))
+                    .route("/info", web::get().to(user_info)),
+            )
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
@@ -35,7 +49,7 @@ mod tests {
     use super::*;
 
     #[actix_web::test]
-    async fn test_index_get() {
+    async fn test_getting_started_get() {
         let app = test::init_service(App::new().service(hello)).await;
         let request = test::TestRequest::default()
             .insert_header(ContentType::plaintext())
@@ -45,5 +59,39 @@ mod tests {
         let body_bytes = test::read_body(response).await;
         let body_str = std::str::from_utf8(&body_bytes).unwrap();
         assert_eq!(body_str, "Hello world!");
+    }
+
+    #[actix_web::test]
+    async fn test_getting_started_scope_all_get() {
+        let app = test::init_service(
+            App::new().service(web::scope("/users").route("/all", web::get().to(all_users))),
+        )
+        .await;
+        let request = test::TestRequest::default()
+            .uri("/users/all")
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(response.status().is_success());
+        let body_bytes = test::read_body(response).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, "All users!");
+    }
+
+    #[actix_web::test]
+    async fn test_getting_started_scope_info_get() {
+        let app = test::init_service(
+            App::new().service(web::scope("/users").route("/info", web::get().to(user_info))),
+        )
+        .await;
+        let request = test::TestRequest::default()
+            .uri("/users/info")
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(response.status().is_success());
+        let body_bytes = test::read_body(response).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, "Info on users!");
     }
 }
