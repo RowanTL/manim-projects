1910e41c0044a4320e80c0fd70de48a29f864065
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-11-29 13:58:53 -0600

    06_custom_response, need to test

---
+++ actix_web_presentation/src/bin/06_custom_response.rs
@@ -0,0 +1,37 @@
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Responder, Result, body::BoxBody, get,
+    http::header::ContentType, web,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: &'static str,
+}
+
+// Responder
+impl Responder for MyObj {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        HttpResponse::Ok()
+            .content_type(ContentType::json())
+            .body(body)
+    }
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    MyObj { name: "user" }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
