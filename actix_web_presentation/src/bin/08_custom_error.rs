e79be9dbd7321b918746a092776cf5c2bbda0bba
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2025-11-30 11:21:34 -0600

    08 custom error done

--- actix_web_presentation/src/bin/08_custom_error.rs
+++ actix_web_presentation/src/bin/08_custom_error.rs
@@ -6,6 +6,7 @@ use derive_more::derive::{Display, Error};
 
 #[derive(Debug, Display, Error)]
 enum MyError {
+    // display macro from derive_more crate
     #[display("internal error")]
     InternalError,
 
@@ -32,15 +33,97 @@ impl error::ResponseError for MyError {
     }
 }
 
-#[get("/")]
-async fn index() -> Result<&'static str, MyError> {
+#[get("/bad")]
+async fn bad_client() -> Result<&'static str, MyError> {
     Err(MyError::BadClientData)
+}
+
+#[get("/internal")]
+async fn internal_error() -> Result<&'static str, MyError> {
+    Err(MyError::InternalError)
+}
+
+#[get("/timeout")]
+async fn timeout() -> Result<&'static str, MyError> {
+    Err(MyError::Timeout)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(bad_client)
+            .service(timeout)
+            .service(internal_error)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{App, http::header::ContentType, test};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_custom_bad_client() {
+        let app = test::init_service(
+            App::new()
+                .service(bad_client)
+                .service(timeout)
+                .service(internal_error),
+        )
+        .await;
+        let request = test::TestRequest::default()
+            .uri("/bad")
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(!response.status().is_success());
+        let body_bytes = test::read_body(response).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, "bad request");
+    }
+
+    #[actix_web::test]
+    async fn test_custom_timeout() {
+        let app = test::init_service(
+            App::new()
+                .service(bad_client)
+                .service(timeout)
+                .service(internal_error),
+        )
+        .await;
+        let request = test::TestRequest::default()
+            .uri("/timeout")
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(!response.status().is_success());
+        let body_bytes = test::read_body(response).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, "timeout");
+    }
+
+    #[actix_web::test]
+    async fn test_custom_internal_error() {
+        let app = test::init_service(
+            App::new()
+                .service(bad_client)
+                .service(timeout)
+                .service(internal_error),
+        )
+        .await;
+        let request = test::TestRequest::default()
+            .uri("/internal")
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let response = test::call_service(&app, request).await;
+        assert!(!response.status().is_success());
+        let body_bytes = test::read_body(response).await;
+        let body_str = std::str::from_utf8(&body_bytes).unwrap();
+        assert_eq!(body_str, "internal error");
+    }
 }
