179ccf5ed43177e49d217a8696c9d7980c0e069e
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-01-26 16:37:49 -0600

    good progress. Will come back to this later

--- injective_surjective_intuition/main.py
+++ injective_surjective_intuition/main.py
@@ -1,5 +1,7 @@
+import itertools
 from typing import Final
 
+import numpy as np
 from manim import *
 
 GLOBAL_SCALE: Final[float] = 0.40
@@ -8,38 +10,87 @@ TEXT_SCALE: Final[float] = 0.40
 
 # Scene that shows both injectivity and bijectivity together
 # Hence the funny "UnBijectivity"
-class UnBijectiveScene(ThreeDScene):
+class UnBijectiveScene(Scene):
     def construct(self):
-        # Scene Setup
-        self.set_camera_orientation(phi=2 * PI / 5, theta=PI / 5)
-        self.begin_ambient_camera_rotation()
+        ## Scene Setup
+        # ...
 
-        # Definitions
+        ## Definitions
 
-        # Axes that will contain a cube getting "squished" into a
-        # plane by a function. After cube gets squished, bring back
-        # to 3D. Probably with a different color.
-        axes: ThreeDAxes = ThreeDAxes(x_range=(-3, 3), y_range=(-3, 3)).scale(
-            GLOBAL_SCALE
+        # Axes that demonstrates the injective and surject functions.
+        axes: Axes = Axes(
+            x_range=[-3, 3], y_range=[-3, 3], x_length=4, y_length=4, tips=False
         )
 
-        # Cube to place on axes and transform into arbitrary 2d plane in
-        # 3d space
-        injective_cylinder: Cylinder = Cylinder(
-            radius=1,
-            height=3,
-            fill_opacity=0.75,
-            fill_color=BLUE,
-            checkerboard_colors=False,
+        # Linear Map (Transformation) that takes a number in R^2 and places it in R
+        # Show at top of the screen
+        lin_map: MathTex = MathTex(r"T: \mathbb{R}^2 \rightarrow \mathbb{R}").to_edge(
+            UP
         )
 
-        # The plane that the injective cylinder transforms into.
-        # injective_plane: Surface = Surface(
+        # Injectivity claim
+        injectivity_claim: Tex = (
+            Tex(
+                r"T injective $\Leftrightarrow$ T maps \\ distinct inputs to distinct outputs"
+            )
+            .next_to(lin_map, DOWN)
+            .scale(0.5)
+        )
 
-        # )
+        # The actual equation of the linear map
+        lin_map_equation: MathTex = (
+            MathTex(r"T = x_1").next_to(lin_map, DOWN).scale(0.9)
+        )
 
-        # Animations
-        self.play(FadeIn(axes))
-        self.wait(2)
-        self.play(FadeIn(injective_cylinder))
-        self.wait(2)
+        # Dots in R^2 to be mapped into R
+        injective_dots = VGroup()
+        injective_dots.add(Dot(axes.c2p(1, 2), color=BLUE))
+        injective_dots.add(Dot(axes.c2p(1, -2), color=BLUE))
+
+        # Line that injective_dots gets mapped into
+        r_1_line = Line(axes.c2p(-3, 0), axes.c2p(3, 0), color=YELLOW, stroke_width=2)
+
+        # Lines that connect the dots to the r_1_line
+        injective_lines: VGroup = VGroup()
+        for dot in injective_dots:
+            line: Line = Line(
+                dot.get_center(), axes.c2p(1, 0), color=PURPLE, stroke_width=2
+            )
+            injective_lines.add(line)
+
+        # Dot in which the two lines in injective_lines join at in R
+        r_1_dot: Dot = Dot(axes.c2p(1, 0), color=PURPLE)
+
+        ## Animations
+        # Write function down
+        self.play(Write(lin_map))
+        self.wait(1)
+
+        # Show statement attempting to "prove"
+        self.play(Write(injectivity_claim))
+        self.wait(1)
+        self.play(Unwrite(injectivity_claim))
+
+        # Show axes for a "visual proof"
+        self.play(Write(axes))
+        self.wait(1)
+
+        # Write dots to screen. Will be morphed into 2d later.
+        self.play(Write(injective_dots))
+        self.wait(1)
+
+        # The equation used to map the dots onto the real line.
+        # Placed below the T: R^2 to R tex at the top of the screen
+        self.play(Write(lin_map_equation))
+        self.wait(1)
+
+        # Write the R line on top of the axes
+        self.play(Write(r_1_line))
+        self.wait(1)
+
+        # Show that both points correspond to one location in R.
+        # Therefore, not injective
+        self.play(Write(injective_lines))
+        self.play(Write(r_1_dot))
+        self.play(Unwrite(injective_lines))
+        self.wait(1)
