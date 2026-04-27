ea0230a26135a26d15c6da399cfcc358f912b17b
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-27 15:43:29 -0500

    ProperPresentSolutionSlide done

--- comp_2_wp_4/main.py
+++ comp_2_wp_4/main.py
@@ -212,32 +212,117 @@ class ProperPresentSolutionSlide(Slide):
     def construct(self):
         # declarations
         in_class_solution_text: Text = (
-            Text("Solution 1").set_color(BLUE).to_edge(UP).scale(0.75)
-        )
-        stick_teacher_tie: SVGMobject = (
-            SVGMobject("images/stick-person-with-tie.svg").set_color(WHITE).scale(0.75)
-        )
-        stick_students_vgroup: VGroup = (
-            VGroup(
-                *[
-                    SVGMobject("images/stick-person.svg").set_color(WHITE).scale(0.4)
-                    for _ in range(14)
-                ]
-            )
-            .arrange_in_grid(rows=2, cols=7)
-            .to_edge(DOWN)
+            Text("Solution Part 1").set_color(BLUE).to_edge(UP).scale(0.75)
         )
         board_rect: Rectangle = Rectangle(height=2, width=10).next_to(
             in_class_solution_text, DOWN
         )
-        # move stick_teacher_tie
-        stick_teacher_tie.next_to(board_rect, DOWN)
+        stick_teacher_tie: SVGMobject = (
+            SVGMobject("images/stick-person-with-tie.svg").set_color(WHITE).scale(0.75)
+        ).next_to(board_rect, DOWN)
+        stick_students_vgroup: VGroup = (
+            VGroup(
+                *[
+                    SVGMobject("images/stick-person.svg").set_color(WHITE).scale(0.4)
+                    for _ in range(20)
+                ]
+            )
+            .arrange_in_grid(rows=2, cols=10)
+            .next_to(stick_teacher_tie, DOWN)
+        )
+        harm_perception_brace: Brace = Brace(
+            stick_students_vgroup, direction=RIGHT, color=RED
+        )
+        harm_perception_text: Paragraph = (
+            Paragraph("Harm", "Perceptions", alignment="left")
+            .set_color(RED)
+            .scale(0.6)
+            .next_to(harm_perception_brace, RIGHT)
+        )
+        harm_perception_vgroup: VGroup = VGroup(
+            harm_perception_brace, harm_perception_text
+        )
+        # Unhyped citation
+        unhyped_citation: Text = (
+            Text(
+                "Glasser, Allison M., et al. “Effect of a State-Level Vaping Prevention Campaign on Beliefs and Behaviors in Young People.” Substance Use & Misuse, vol. 60, no. 5, 2025, pp. 659–68, https://doi.org/10.1080/10826084.2024.2446741."
+            )
+            .set_color(GREY)
+            .scale(0.15)
+            .to_edge(DOWN)
+        )
+
+        # Use with Skran example
+        specific_example_text: Text = (
+            Text("Use specific examples")
+            .set_color(GREEN)
+            .scale(0.8)
+            .move_to(board_rect)
+        )
+        # skran citiation
+        skran_citation: Text = (
+            Text(
+                "Skran, Sarah R. “The Effectiveness of Anti-Vaping Health Communication Campaigns among High School and College Students in the U.S.” Frontiers in Public Health, vol. 13, 5 Jan. 2026, https://doi.org/10.3389/fpubh.2025.1676181."
+            )
+            .set_color(GREY)
+            .scale(0.15)
+            .to_edge(DOWN)
+        )
 
         # animations
+        self.play(Write(in_class_solution_text))
+        self.wait(0.25)
+
+        self.next_slide()
+
         self.play(
             Write(stick_teacher_tie),
             Write(stick_students_vgroup),
             Write(board_rect),
-            Write(in_class_solution_text),
+        )
+        self.wait(0.25)
+
+        # harm perceptions portion
+        self.next_slide()
+
+        self.play(Write(unhyped_citation), Write(harm_perception_vgroup))
+
+        # remove unhyped citation and harm_perceptions_vgroup
+        self.next_slide()
+
+        self.play(
+            Unwrite(unhyped_citation), Unwrite(harm_perception_vgroup), run_time=0.75
+        )
+        self.wait(0.25)
+
+        # remove harm perceptions text and citation
+        # Move onto ways to properly educate students with specific examples
+        self.next_slide()
+
+        self.play(Write(specific_example_text), Write(skran_citation))
+
+        self.next_slide()
+
+        self.play(
+            Transform(
+                specific_example_text,
+                Text("Blisters of air form in the lungs")
+                .set_color(GREEN)
+                .scale(0.8)
+                .move_to(board_rect),
+            )
+        )
+
+        # Clean up for indirect advertising campaign
+        self.next_slide()
+
+        self.play(
+            Unwrite(in_class_solution_text),
+            Unwrite(board_rect),
+            Unwrite(stick_teacher_tie),
+            Unwrite(stick_students_vgroup),
+            Unwrite(skran_citation),
+            Unwrite(specific_example_text),
+            run_time=0.5,
         )
         self.wait(0.25)
