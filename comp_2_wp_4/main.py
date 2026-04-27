0e4b4c101a5b83995a981df85b1abb0ecdad8de3
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-04-26 19:28:52 -0500

    IntroSlide done and part of WhyProblemSlide done

--- comp_2_wp_4/main.py
+++ comp_2_wp_4/main.py
@@ -1,16 +1,40 @@
 from manim import *
-from manim_slides import Slide
+from manim_slides.slide.manim import Slide
+
 
 # Slide 0
 class IntroSlide(Slide):
     def construct(self):
-        # circle = Circle()  # create a circle
-        # circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+        # declarations
+        title_text: Text = Text("Dissuading E-Cigarette Use").to_edge(UP)
+        name_text: Text = Text("Rowan Torbitzky-Lane").scale(0.5).set_color(BLUE)
 
-        # square = Square()  # create a square
-        # square.flip(RIGHT)  # flip horizontally
-        # square.rotate(-3 * TAU / 8)  # rotate a certain amount
+        # animations
+        self.play(Write(title_text))
 
-        # self.play(Create(square))  # animate the creation of the square
-        # self.play(Transform(square, circle))  # interpolate the square into the circle
-        # self.play(FadeOut(square))  # fade out animation
+        self.next_slide()
+
+        self.play(Write(name_text))
+
+        self.next_slide()
+
+        self.play(Unwrite(title_text), Unwrite(name_text), run_time=0.5)
+        self.wait(0.25)
+
+        self.next_slide()
+
+
+# Slide 1
+class WhyProblemSlide(Slide):
+    def construct(self):
+        # declarations
+        juul_pod_image: ImageMobject = ImageMobject("images/juul_pod.jpg").scale(2.0)
+        cigarette_image: SVGMobject = SVGMobject("images/cigarette.svg").scale(0.1)
+
+        # animations
+        self.play(FadeIn(juul_pod_image))
+
+        self.next_slide()
+
+        self.play(juul_pod_image.animate.to_corner(LEFT))
+        self.play(Write(cigarette_image))
